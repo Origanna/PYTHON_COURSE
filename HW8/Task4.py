7887311c2a350382225e2e5d11443e38724d5d4c
Olga <origanna@yandex.ru>
2025-06-21 14:57:08 +0300

    Добавлены HW8 и файлы к HW8

---
+++ HW8/Task4.py
@@ -0,0 +1,30 @@
+# Задание 4.
+
+# Определяем английский алфавит
+english_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+total_letters = 0
+
+# Открываем файл text.txt и считываем текст
+with open("text.txt", "r") as file:
+    text = file.read().lower() # Приводим текст к нижнему регистру
+
+# Создаем словарь для подсчета количества каждой буквы
+letter_count = {letter: 0 for letter in english_alphabet}
+
+# Подсчитываем количество вхождений каждой буквы
+for char in text:
+    if char in english_alphabet:
+        letter_count[char] += 1
+        total_letters += 1
+
+# Вычисляем частоту встречаемости каждой буквы
+letter_freq = {letter: (count / total_letters) for letter, count in
+letter_count.items() if count > 0}
+
+# Сортируем буквы по убыванию частоты и по алфавиту при равенстве частоты
+sorted_letters = sorted(letter_freq.items(), key=lambda x: (-x[1], x[0]))
+
+# Записываем результат в файл analysis.txt
+with open("analysis.txt", "w") as file:
+    for letter, freq in sorted_letters:
+        file.write(f"{letter} {freq:.3f}\n")
